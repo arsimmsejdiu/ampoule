79fe826b8b58b70be11a89b090128883623b12b3
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-05-10 09:19:21 +0200

    fixed the button for creating the light bulp

--- home.php
+++ home.php
@@ -56,8 +56,9 @@ if (isset($_GET['did'])) {
         <?php } ?>
     </tbody>
 </table>
-<div class="form-group btn-h">
-    <button onclick="myFunction()" class="btn btn-success btn-lg btn-block btn-h"><a href="create.php">Ajoute D'Ampoule</a></button>
+<br />
+<div class="form-group">
+    <button onclick="myFunction()" class="btn btn-success btn-lg btn-block"><a href="create.php">Ajoute D'Ampoule</a></button>
 </div>
 <?php
 require('footer.php')
